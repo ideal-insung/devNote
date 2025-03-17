53cde9770cb7e84b11214d9f9e00b7efba5f5757
ideal-insung <82872175+ideal-insung@users.noreply.github.com>
2025-03-17 22:20:01 +0900

    stack 자료구조
    배열 리스트로 구현

---
+++ StackMain.java
@@ -0,0 +1,21 @@
+package data_structure;
+
+import java.util.Queue;
+
+public class StackMain {
+
+	public static void main(String[] args) {
+		LinkedListStack lls = new LinkedListStack();
+
+		lls.push("A");
+		lls.push("B");
+		lls.push("C");
+		lls.push("D");
+		
+		while(!lls.isEmty()) {
+			System.out.print("pop된 데이터 :");
+			System.out.println(lls.pop());
+		}
+		
+	}
+}
